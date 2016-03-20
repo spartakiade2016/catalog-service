8da3803bc83e7066a7e5e9d10d675a51faaf2258
Thomas <Thomas@DevBook>
2016-03-20 17:04:05 +0100

    now json is correct

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ const PORT = process.env.PORT || 3000;
 const SHUTDOWN_TIMEOUT = process.env.SHUTDOWN_TIMEOUT || 10000;
 const SERVICE_CHECK_HTTP = process.env.SERVICE_CHECK_HTTP || '/healthcheck';
 const SERVICE_ENDPOINTS = process.env.SERVICE_ENDPOINTS || '/endpoints';
-const DISCOVERY_SERVERS = process.env.DISCOVERY_SERVERS
-  ? process.env.DISCOVERY_SERVERS.split(',')
-  : ['http://???.???.???.001:8500', 'http://???.???.???.002:8500', 'http://???.???.???.003:8500'];
+const DISCOVERY_SERVERS = process.env.DISCOVERY_SERVERS ? process.env.DISCOVERY_SERVERS.split(',') : ['http://???.???.???.001:8500', 'http://???.???.???.002:8500', 'http://???.???.???.003:8500'];
 
 // Create a new express app
 const app = express();
@@ -24,47 +22,50 @@ const app = express();
 app.use(cors());
 
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 app.use(bodyParser.json());
 
 // Add health check endpoint
-app.get(SERVICE_CHECK_HTTP, (req, res) => res.send({ uptime: process.uptime() }));
+app.get(SERVICE_CHECK_HTTP, (req, res) => res.send({
+  uptime: process.uptime()
+}));
 
 // Add metadata endpoint
 app.get(SERVICE_ENDPOINTS, endpoints());
 
 // Add all other service routes
 app.get('/products', (req, res) => {
-    res.send({
-        products: getProducts()
-    });
+  res.send({
+    products: getProducts()
+  });
 });
 
 app.post('/addProduct', (req, res) => {
-    addProduct(req.body.product);
+  addProduct(req.body.product);
 
-    res.status(201);
+  res.status(201);
 });
 
 const getProducts = () => {
-    var products = JSON.parse(fs.readFileSync(__dirname + "/products.json", "utf8"));
-
-    return products;
+  var products = JSON.parse(fs.readFileSync(__dirname + "/products.json", "utf8"));
+  return products;
 };
 
 const addProduct = (product) => {
-    var products = getProducts();
-    products.products.push(product);
+  var products = getProducts();
+  products.products.push(product);
 
-    fs.writeFileSync(__dirname + "/products.json", JSON.stringify(products), "utf8");
+  fs.writeFileSync(__dirname + "/products.json", JSON.stringify(products), "utf8");
 };
 
 // Start the server
 const server = app.listen(PORT, () => console.log(`Service listening on port ${PORT} ...`));
 
 // Enable graceful server shutdown when process is terminated
-gracefulShutdown(server, { timeout: SHUTDOWN_TIMEOUT });
+gracefulShutdown(server, {
+  timeout: SHUTDOWN_TIMEOUT
+});
 
 
 
